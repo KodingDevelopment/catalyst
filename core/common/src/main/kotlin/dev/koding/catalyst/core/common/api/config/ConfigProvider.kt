652dcb8b58b1e894b0d60eb3552fe6e86785d0a9
Koding Dev <kodingdev@pm.me>
2022-12-26 18:00:03 +1100

    fix(config): linting

--- core/common/src/main/kotlin/dev/koding/catalyst/core/common/api/config/ConfigProvider.kt
+++ core/common/src/main/kotlin/dev/koding/catalyst/core/common/api/config/ConfigProvider.kt
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 @file:Suppress("unused")
 
 package dev.koding.catalyst.core.common.api.config
--- core/common/src/main/kotlin/dev/koding/catalyst/core/common/api/config/impl/JsonConfigProvider.kt
+++ core/common/src/main/kotlin/dev/koding/catalyst/core/common/api/config/impl/JsonConfigProvider.kt
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 @file:OptIn(ExperimentalSerializationApi::class)
 
 package dev.koding.catalyst.core.common.api.config.impl
