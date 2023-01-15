3d0b8a5f0a987642d199e3bb00c32274e224a744
Koding Dev <kodingdev@pm.me>
2023-01-16 03:59:36 +1100

    chore(platform): linting

--- core/paper/src/main/kotlin/dev/koding/catalyst/core/paper/api/platform/entity/PaperPlatformEntity.kt
+++ core/paper/src/main/kotlin/dev/koding/catalyst/core/paper/api/platform/entity/PaperPlatformEntity.kt
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 package dev.koding.catalyst.core.paper.api.platform.entity
 
 import dev.koding.catalyst.core.common.api.platform.entity.PlatformEntity
