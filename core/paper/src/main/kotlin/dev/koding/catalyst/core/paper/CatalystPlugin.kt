ad3bf21df5e24df01c8b1a920652c0530bab6868
Stella Inwood <hello@koding.dev>
2023-04-27 19:10:57 +1000

    chore(paper): remove debugging

--- core/paper/src/main/kotlin/dev/koding/catalyst/core/paper/CatalystPlugin.kt
+++ core/paper/src/main/kotlin/dev/koding/catalyst/core/paper/CatalystPlugin.kt
@@ -17,18 +17,12 @@
  */
 package dev.koding.catalyst.core.paper
 
-import dev.koding.catalyst.core.common.api.platform.SchedulersImpl
-import dev.koding.catalyst.core.common.api.platform.sided.PlatformServerImpl
-import dev.koding.catalyst.core.common.api.platform.world.BlockMetadata
 import dev.koding.catalyst.core.common.injection.component.Bootstrap
 import dev.koding.catalyst.core.common.util.ext.logExecutionTime
-import dev.koding.catalyst.core.common.util.ext.on
 import dev.koding.catalyst.core.paper.api.platform.PaperPlatform
 import dev.koding.catalyst.core.paper.loader.PaperLoader
 import dev.koding.catalyst.core.paper.util.SpigotObf
 import mu.KLogger
-import net.kyori.adventure.text.Component
-import org.bukkit.event.player.PlayerJoinEvent
 import org.kodein.di.DI
 import org.kodein.di.DIAware
 import org.kodein.di.bind
@@ -52,24 +46,5 @@ class CoreBootstrap(override val di: DI) : DIAware, Bootstrap {
 
         // Load obfuscation mappings
         logger.logExecutionTime({ info { "Loaded obfuscation mappings in ${it}ms" } }) { SpigotObf.load() }
-
-        PlatformServerImpl.worlds.forEach {
-            println(it.name)
-            val chunk = it.getChunk(0, 0) ?: return@forEach
-            for (i in it.minY..it.maxY) {
-                chunk.setBlock(0, i, 0, BlockMetadata.AIR)
-            }
-        }
-
-        SchedulersImpl.sync.schedule(0L, 500L) {
-            PlatformServerImpl.players.forEach {
-                it.teleport(it.position.add(0.0, 0.5, 0.0))
-                it.sendMessage(Component.text("lol"))
-            }
-        }
-
-        on<PlayerJoinEvent> {
-            println(it.player)
-        }
     }
 }
