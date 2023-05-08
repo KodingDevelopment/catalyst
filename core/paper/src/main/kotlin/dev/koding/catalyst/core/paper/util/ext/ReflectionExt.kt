7478ee23edda8afac64ac227401d8d2d98cee99c
Stella Inwood <hello@koding.dev>
2023-05-08 17:17:46 +1000

    feat(core): update reflection ext to get raw fields / methods

--- core/paper/src/main/kotlin/dev/koding/catalyst/core/paper/util/ext/ReflectionExt.kt
+++ core/paper/src/main/kotlin/dev/koding/catalyst/core/paper/util/ext/ReflectionExt.kt
@@ -20,7 +20,45 @@ package dev.koding.catalyst.core.paper.util.ext
 import dev.koding.catalyst.core.common.util.ext.Reflector
 import dev.koding.catalyst.core.paper.util.SpigotObf
 import java.lang.reflect.Field
+import java.lang.reflect.Method
 import java.lang.reflect.Modifier
+
+/**
+ * Resolve a field in the given class.
+ *
+ * @param name The name of the field to resolve.
+ * @return The field if found.
+ */
+fun <T : Any> Class<T>.findObfuscatedField(name: String): Field? {
+    // Re-obfuscate the field name if necessary
+    val fieldName = SpigotObf.getClassSpigot(this.name)?.getFieldMojang(name)?.name?.spigot ?: name
+    return runCatching { Reflector.getField(this, fieldName) }.getOrNull()
+}
+
+/**
+ * Resolve a method in the given class.
+ *
+ * @param name The name of the method to resolve.
+ * @param args The arguments of the method to resolve.
+ * @return The method if found.
+ *
+ * FIXME: This method doesn't account for the arguments matching in the case of
+ *        methods with the same name but different arguments. We need to find a
+ *        way to resolve the correct method using the descriptor.
+ */
+fun <T : Any> Class<T>.findObfuscatedMethod(name: String, vararg args: Class<*>): Method? {
+    val methodName = SpigotObf.getClassSpigot(this::class.java.name)
+        ?.methods?.firstOrNull { it.name.mojang == name }?.name?.spigot
+        ?: name
+
+    return runCatching {
+        Reflector.getMethod(
+            this::class.java,
+            methodName,
+            *args.map { it::class.java }.toTypedArray()
+        )
+    }.getOrNull()
+}
 
 /**
  * Resolves a field for the given path.
@@ -31,18 +69,14 @@ import java.lang.reflect.Modifier
  * @return The field if found
  * @throws NoSuchFieldException If the field could not be found.
  */
-@Suppress("UNCHECKED_CAST")
-fun <T : Any> Any.resolveReflectionFieldsAs(vararg names: String): T? {
+@Suppress("UNCHECKED_CAST", "unused")
+fun <T : Any> Any.resolveObfuscatedFieldAs(vararg names: String): T? {
     var currentObject: Any = this
     var currentField: Field?
 
     for (name in names) {
-        // Re-obfuscate the field name if necessary
-        val fieldName = SpigotObf.getClassSpigot(currentObject::class.java.name)
-            ?.getFieldMojang(name)?.name?.spigot
-            ?: name
-
-        currentField = Reflector.getField(currentObject::class.java, fieldName)
+        currentField = currentObject::class.java.findObfuscatedField(name)
+            ?: throw NoSuchFieldException("Could not find field $name in ${currentObject::class.java.name}")
 
         // Check if static
         currentObject = if (currentField.modifiers and Modifier.STATIC != 0) currentField.get(null)
@@ -60,11 +94,8 @@ fun <T : Any> Any.resolveReflectionFieldsAs(vararg names: String): T? {
  * @return The result of the method invocation.
  */
 @Suppress("UNCHECKED_CAST", "unused")
-fun <T : Any> Any.invokeReflectionMethodAs(name: String, vararg args: Any): T? {
-    val methodName = SpigotObf.getClassSpigot(this::class.java.name)
-        ?.methods?.firstOrNull { it.name.mojang == name }?.name?.spigot
-        ?: name
-
-    val method = Reflector.getMethod(this::class.java, methodName)
+fun <T : Any> Any.invokeObfuscatedMethod(name: String, vararg args: Any): T? {
+    val method = this::class.java.findObfuscatedMethod(name, *args.map { it::class.java }.toTypedArray())
+        ?: throw NoSuchMethodException("Could not find method $name in ${this::class.java.name}")
     return method.invoke(this, args) as? T
 }
